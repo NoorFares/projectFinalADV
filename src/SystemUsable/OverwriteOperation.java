bdbd372fcc5aa3a18290a7d6d6e81cf59047b138
git config --global user.namNoorFares <s12028537@stu.najah.edu>
2022-12-25 17:49:24 +0200

    change the refactory

--- src/SystemUsable/OverwriteOperation.java
+++ src/SystemUsable/OverwriteOperation.java
@@ -1,11 +1,10 @@
 package SystemUsable;
-import AuthenticationSystem.Authentication;
 import DBConnection.DBConnection;
 import DBConnection.CloseConnection;
 import Encryption.Encryptable;
 import Encryption.FileNameEncryptor;
 import Encryption.FileNameEncryptorAdapter;
-import FileRepositroy.GetType;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -15,7 +14,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Logger;
-
 public class OverwriteOperation implements Operation{
     private static final Logger logger = Logger.getLogger(OverwriteOperation.class.getName());
     private static final int FILE_NAME_INDEX =1;
@@ -24,6 +22,7 @@ public class OverwriteOperation implements Operation{
     private static final int FILE_TYPE_INDEX =3;
     private static final int FILE_LENGTH_INDEX =4;
     private static final int FILE_DATA_INDEX =5;
+    private static final int FILE_DATA_INDEXi =1;
     private static final int FILE_ENCRYPTED_INDEX =6;
     private static final String OVERWRITE_OPERATION ="Overwrite operation";
     private static final String CLOSING_DATABASE ="Closing database connection";
@@ -54,23 +53,23 @@ public class OverwriteOperation implements Operation{
     }
 
     public static void updateFile(Connection conn, String fileName, InputStream inputStream) throws SQLException {
-        PreparedStatement pstmt = conn.prepareStatement("UPDATE fields SET fileData = ?, fileVersion = fileVersion + 1 WHERE fileName = ?");
-        pstmt.setBinaryStream(FILE_DATA_INDEX, inputStream);
-        pstmt.setString(FILE_NAME_INDEX_FOR_UPDATE, fileName);
-        pstmt.executeUpdate();
+        PreparedStatement preparedStatement = conn.prepareStatement("UPDATE fields SET fileData = ?, fileVersion = fileVersion + 1 WHERE fileName = ?");
+        preparedStatement.setBinaryStream(FILE_DATA_INDEXi, inputStream);
+        preparedStatement.setString(FILE_NAME_INDEX_FOR_UPDATE, fileName);
+        preparedStatement.executeUpdate();
     }
 
     public static void insertFile(Connection conn, File file,InputStream inputStream,String encryptedFileName) throws SQLException {
 
         String sql ="INSERT INTO fields( fileName, filePath, fileType,fileSize, fileVersion, fileData,encryptedFile) VALUES (?, ?, ?, ?, 1, ?, ?)";
-        PreparedStatement pstmt = conn.prepareStatement(sql);
-        pstmt.setString(FILE_NAME_INDEX, file.getName());
-        pstmt.setString(FILE_PATH_INDEX, file.getPath());
-        pstmt.setString(FILE_TYPE_INDEX, GetType.getType(file));
-        pstmt.setLong(FILE_LENGTH_INDEX, file.length());
-        pstmt.setBinaryStream(FILE_DATA_INDEX, inputStream);
-        pstmt.setString(FILE_ENCRYPTED_INDEX, encryptedFileName);
-        pstmt.executeUpdate();
+        PreparedStatement preparedStatement = conn.prepareStatement(sql);
+        preparedStatement.setString(FILE_NAME_INDEX, file.getName());
+        preparedStatement.setString(FILE_PATH_INDEX, file.getPath());
+        preparedStatement.setString(FILE_TYPE_INDEX, GetType.getType(file));
+        preparedStatement.setLong(FILE_LENGTH_INDEX, file.length());
+        preparedStatement.setBinaryStream(FILE_DATA_INDEX, inputStream);
+        preparedStatement.setString(FILE_ENCRYPTED_INDEX, encryptedFileName);
+        preparedStatement.executeUpdate();
     }
 }
 
